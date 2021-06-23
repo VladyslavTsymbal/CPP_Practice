b49167bb88588d397cc85a0c1365d99b365fc3d3
Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>
2021-06-23 15:26:33 +0300

    Vector: Add size, capacity funcs
    Signed-off-by: Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -31,6 +31,9 @@ class Vector
 		const_iterator end() const noexcept;
 		const_iterator cend() const noexcept;
 
+		size_type size() const noexcept;
+		size_type capacity() const noexcept;
+
 	private:
 		std::unique_ptr<T[]> m_data = nullptr;
 		iterator m_begin            = nullptr;
@@ -50,6 +53,12 @@ struct Vector<T, Allocator>::Iterator :
 	bool operator < (const Iterator other)
 	{
 		return m_p < other.m_p;
+	}
+
+	typename Iterator::difference_type
+	operator -(const Iterator other) const
+	{
+		return m_p - other.m_p;
 	}
 
 	private:
@@ -96,4 +105,18 @@ typename Vector<T, Allocator>::const_iterator
 Vector<T, Allocator>::cend() const noexcept
 {
 	return end();
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::size_type
+Vector<T, Allocator>::size() const noexcept
+{
+	return m_end - m_begin;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::size_type
+Vector<T, Allocator>::capacity() const noexcept
+{
+	return m_capacity - m_begin;
 }
