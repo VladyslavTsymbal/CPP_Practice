be3d06d8d49886c452932fb19a4f71640364ae5e
Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>
2021-06-23 14:42:01 +0300

    Vector: Base iterator implementation
    Add iterators "begin", "end", "capacity".
    Base implementation of vector iterator. Will be extended in future.
    
    Signed-off-by: Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <memory>
+
 template <typename T, typename Allocator = std::allocator<T>>
 class Vector
 {
 	public:
+		template <typename A>
+		struct Iterator;
+
 		using value_type      = T;
 		using allocator_type  = Allocator;
 		using size_type       = typename Allocator::size_type;
@@ -12,12 +17,33 @@ class Vector
 		using const_reference = typename Allocator::const_reference;
 		using pointer         = typename Allocator::pointer;
 		using const_pointer   = typename Allocator::const_pointer;
+		using iterator        = Iterator<value_type>;
+		using const_iterator  = Iterator<const value_type>;
 
 	public:
-		Vector();
+		Vector() = default;
+
+	private:
+		std::unique_ptr<T[]> m_data = nullptr;
+		iterator m_begin            = nullptr;
+		iterator m_end              = nullptr;
+		iterator m_capacity         = nullptr;
 };
 
 template <typename T, typename Allocator>
-Vector<T, Allocator>::Vector()
+template <typename A>
+struct Vector<T, Allocator>::Iterator :
+	public std::iterator<std::random_access_iterator_tag, A>
 {
-}
+	Iterator(pointer p) : m_p(p)
+	{
+	}
+
+	bool operator < (const Iterator other)
+	{
+		return m_p < other.m_p;
+	}
+
+	private:
+		pointer m_p;
+};
