dd65efbea50f2b0d0f143f0f4e02b7d8129e67d2
Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>
2021-06-23 13:59:23 +0300

    Vector: Define a few public type alliases
    Signed-off-by: Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
-#include <iostream>
-
 template <typename T, typename Allocator = std::allocator<T>>
 class Vector
 {
+	public:
+		using value_type      = T;
+		using allocator_type  = Allocator;
+		using size_type       = typename Allocator::size_type;
+		using difference_type = typename Allocator::difference_type;
+		using reference       = typename Allocator::reference;
+		using const_reference = typename Allocator::const_reference;
+		using pointer         = typename Allocator::pointer;
+		using const_pointer   = typename Allocator::const_pointer;
+
 	public:
 		Vector();
 };
@@ -12,5 +20,4 @@ class Vector
 template <typename T, typename Allocator>
 Vector<T, Allocator>::Vector()
 {
-	std::cout << "Hello from Vector!\n";
 }
